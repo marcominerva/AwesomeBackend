5f3fc12c35dd6c70413de153bbdae954cf93d076
Marco Minerva <marco.minerva@gmail.com>
2019-10-24 17:17:34 +0200

    Small fix

--- Src/AwesomeBackend.Common/Models/Requests/LoginRequest.cs
+++ Src/AwesomeBackend.Common/Models/Requests/LoginRequest.cs
@@ -10,7 +10,6 @@ namespace AwesomeBackend.Common.Models.Requests
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
--- Src/AwesomeBackend.Common/Models/Requests/RegisterRequest.cs
+++ Src/AwesomeBackend.Common/Models/Requests/RegisterRequest.cs
@@ -17,11 +17,10 @@ namespace AwesomeBackend.Common.Models.Requests
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Compare(nameof(Password), ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
 }
